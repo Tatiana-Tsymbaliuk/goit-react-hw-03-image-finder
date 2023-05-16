603015fb48b590bec637322ba4cb5ef86d32fe5d
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-05-16 20:06:08 +0300

    create loader

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
-function fetchFoto(name){
-    return fetch(`https://pixabay.com/api/?q=${name}&page=1&key=35097057-211b94425911255e7a1c05e6d&image_type=photo&orientation=horizontal&per_page=12`)
-        .then(res=> res.json())
+const fetchFoto = async (text)=>{
+   const data = await fetch(`https://pixabay.com/api/?q=${text}&page=1&key=35097057-211b94425911255e7a1c05e6d&image_type=photo&orientation=horizontal&per_page=12`)
+        // .then(res=> res.json())
+        return await data.json();
 }
 
 export default fetchFoto;
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -3,25 +3,30 @@ import Loader from 'components/Loader/Loader';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import './ImageGallery.css';
 import Button from 'components/Button/Button';
-import fotoAPI from './src/api'
+import fetchFoto from '../../api/api'
 
 export default class ImageGallery extends React.Component{
   state={
-        fotos: '',
+        fotos:null,
         loading: false,
         error: null,
   }   
-      componentDidUpdate(prevProps, prevState){
+     async componentDidUpdate(prevProps, prevState){
         const nameSearch =this.props.nameSearch
         const prevName = prevProps.nameSearch
         if(prevName!==nameSearch){
-                console.log('update project');
                 this.setState({loading: true})
-                fotoAPI
-                .fetchFoto(nameSearch)
-                .then(data=>this.setState({fotos:data.hits}))
-                .catch(error=>this.setState({error}))
-                .finally(()=>this.setState({loading:false}))
+              try {
+                const {hits} = await fetchFoto(nameSearch)
+                this.setState({fotos: hits})
+              } catch (error) {
+                this.setState({error})
+              }finally{
+                this.setState({loading:false})
+              }
+                
+                // .catch(error=>this.setState({error}))
+                // .(()=>this.setState({loading:false}))
         }
       }  
 
@@ -29,13 +34,15 @@ export default class ImageGallery extends React.Component{
                 const {fotos, loading, error} = this.state
                 const {nameSearch} = this.props
         return(
-          <div>   {error&&<h1>Все пропало...</h1>}    
+          <div>      
         <ul className="ImageGallery">
                {loading&&<Loader/>}
                 {!nameSearch&&<div>Vvedite name foto</div>}
                 {fotos ? ( <ImageGalleryItem items={fotos}/>) : (<div></div>)}
       </ul>
-      {fotos&&<Button/>}
+      {fotos ? ( <Button/>) : (<div></div>)}
+     
+      {error&&<h1>Все пропало...</h1>} 
       </div>  
       
      )} 
